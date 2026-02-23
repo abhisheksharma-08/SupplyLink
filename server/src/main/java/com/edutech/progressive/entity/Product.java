3ca70cb2b59b12b34e6eb653c0ab764a04501f3a
guptasamarth12 <dummy@gmail.com>
2026-02-23 09:50:24 +0000

    SAM

--- server/src/main/java/com/edutech/progressive/entity/Product.java
+++ server/src/main/java/com/edutech/progressive/entity/Product.java
@@ -1,10 +1,7 @@
 package com.edutech.progressive.entity;
 
 public class Product {
-<<<<<<< HEAD
-
-=======
     int id;
     String Name;
->>>>>>> 28e84eb474accf3308d57013bf31b7b84a95580d
+    String dept;
 }
