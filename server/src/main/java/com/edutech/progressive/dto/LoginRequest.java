17922265d9786a433b88768074ef5848e793ae46
Abhishek Kumar <113275276+abhisheksharma-08@users.noreply.github.com>
2026-02-28 09:55:06 +0530

    Update LoginRequest.java

--- server/src/main/java/com/edutech/progressive/dto/LoginRequest.java
+++ server/src/main/java/com/edutech/progressive/dto/LoginRequest.java
@@ -1,23 +1,46 @@
 package com.edutech.progressive.dto;
 
 
-public class LoginRequest {
-   private String username;
-   private String password;
 
-   public String getUsername() {
-      return username;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class LoginResponse {
+   private String token;
+   private String roles;
+   private Integer userId;
+
+   @JsonCreator
+   public LoginResponse(@JsonProperty("token") String token , String roles, Integer userId) {
+      this.token = token;
+      this.roles=roles;
+      this.userId=userId;
    }
 
-   public void setUsername(String username) {
-      this.username = username;
+
+   public String getToken() {
+      return token;
    }
 
-   public String getPassword() {
-      return password;
+   public void setToken(String token) {
+      this.token = token;
+   }
+   public String getRoles()
+   {
+      return this.roles;
    }
 
-   public void setPassword(String password) {
-      this.password = password;
+   public void setRole(String roles)
+   {
+      this.roles = roles;
+   }
+   public void setUserId(Integer userId)
+   {
+      this.userId=userId;
+   }
+
+   public long getUserId()
+   {
+      return this.userId;
    }
 }
