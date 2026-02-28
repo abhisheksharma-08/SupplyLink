85a4f9f15bbd710d95dfb9cdf0ef23647f2f51b6
Abhishek Kumar <113275276+abhisheksharma-08@users.noreply.github.com>
2026-02-28 10:06:05 +0530

    Update InsuranceServiceImpl.java

--- server/src/main/java/com/edutech/progressive/service/impl/InsuranceServiceImpl.java
+++ server/src/main/java/com/edutech/progressive/service/impl/InsuranceServiceImpl.java
@@ -1,5 +1,40 @@
 package com.edutech.progressive.service.impl;
 
-public class InsuranceServiceImpl  {
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.sql.SQLException;
+import java.util.List;
+
+@Service
+public class InsuranceServiceImpl implements InsuranceService {
+
+    @Autowired
+    InsuranceRepository insuranceRepository;
+
+    @Override
+    public List<Insurance> getAllInsurances() throws SQLException {
+        return insuranceRepository.findAll();
+    }
+
+    @Override
+    public int addInsurance(Insurance insurance) throws SQLException {
+        return insuranceRepository.save(insurance).getInsuranceId();
+    }
+
+    @Override
+    public Insurance getInsuranceById(int insuranceId) throws SQLException {
+        return insuranceRepository.findByInsuranceId(insuranceId);
+    }
+
+    @Override
+    public void updateInsurance(Insurance insurance) throws SQLException {
+        insuranceRepository.save(insurance);
+    }
+
+    @Override
+    public void deleteInsurance(int insuranceId) throws SQLException {
+        insuranceRepository.deleteById(insuranceId);
+    }
 }
